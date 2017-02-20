e356f238e644afa7ccd0533a0a04c9a1622bea1d
shreyastawre <tawre.shreyas@gmail.com>
2017-02-19 23:43:56 -0600

    First Commit

---
+++ GitHubRepoTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  GitHubRepoTableViewCell.swift
+//  GithubDemo
+//
+//  Created by Shreyas Tawre on 2/19/17.
+//  Copyright © 2017 codepath. All rights reserved.
+//
+
+import UIKit
+
+class GitHubRepoTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ RepoResultsViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ViewController.swift
+//  GithubDemo
+//
+//  Created by Nhan Nguyen on 5/12/15.
+//  Copyright (c) 2015 codepath. All rights reserved.
+//
+
+import UIKit
+import MBProgressHUD
+
+// Main ViewController
+class RepoResultsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var searchBar: UISearchBar!
+    var searchSettings = GithubRepoSearchSettings()
+
+    var repos: [GithubRepo]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        // Initialize the UISearchBar
+        searchBar = UISearchBar()
+        searchBar.delegate = self
+
+        // Add SearchBar to the NavigationBar
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+
+        // Perform the first search when the view controller first loads
+        doSearch()
+    }
+
+    // Perform the search.
+    fileprivate func doSearch() {
+        
+        var repos: [GithubRepo]!
+
+
+        MBProgressHUD.showAdded(to: self.view, animated: true)
+
+        // Perform request to GitHub API to get the list of repositories
+        GithubRepo.fetchRepos(searchSettings, successCallback: { (newRepos) -> Void in
+            
+            // Print the returned repositories to the output window
+            for repo in newRepos {
+                print(repo)
+            }
+            
+            repos = newRepos
+
+            MBProgressHUD.hide(for: self.view, animated: true)
+            }, error: { (error) -> Void in
+                print(error)
+        })
+    }
+}
+
+// SearchBar methods
+extension RepoResultsViewController: UISearchBarDelegate {
+
+    func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(true, animated: true)
+        return true
+    }
+
+    func searchBarShouldEndEditing(_ searchBar: UISearchBar) -> Bool {
+        searchBar.setShowsCancelButton(false, animated: true)
+        return true
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+    }
+
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchSettings.searchString = searchBar.text
+        searchBar.resignFirstResponder()
+        doSearch()
+    }
+}
